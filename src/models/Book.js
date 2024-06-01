ef1476a1b7d571071f715b195a57f3a01bebcba1
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2024-06-01 14:38:22 +0530

    schema name changed

--- src/models/Book.js
+++ src/models/Book.js
@@ -10,6 +10,6 @@ const BookSchema = new Schema({
   averageRating: { type: Number, default: 0 },
 });
 
-const Book = mongoose.model("Book", BookSchema);
+const Book = mongoose.model("hashbooks", BookSchema);
 
 export default Book;
